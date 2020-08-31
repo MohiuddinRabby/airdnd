d86a222c8113d3eb88a9afe3d93c00f69ac251d3
rabbymohiuddin <rabbymohiuddin@gmail.com>
2020-08-31 23:30:58 +0600

    basic loader & form validation added

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,16 +1,31 @@
 import React from "react";
-
+import { useState } from "react";
+import { useEffect } from "react";
+import planeImg from "../../img/planeOne.gif";
 const Result = () => {
+  const [result, setResult] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://api.icndb.com/jokes")
+      .then((res) => res.json())
+      .then((data) => {
+        setResult(data.value.slice(10, 15));
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <ul>
-        <li>Rsult</li>
-        <li>Rsult</li>
-        <li>Rsult</li>
-        <li>Rsult</li>
-        <li>Rsult</li>
-        <li>Rsult</li>
-      </ul>
+      {loading ? (
+        <div className="text-center">
+          <img
+            src={planeImg}
+            style={{ width: "300px", height: "300px" }}
+            alt=""
+          />
+        </div>
+      ) : (
+        result.map((post) => <li>{post.joke}</li>)
+      )}
     </div>
   );
 };
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,43 +3,97 @@ import { useState } from "react";
 import Result from "../Result/Result";
 import "./Search.css";
 import Banner from "../Banner/Banner";
+import { useForm } from "react-hook-form";
 const Search = () => {
+  const { handleSubmit, register, errors } = useForm();
   const [load, setLoad] = useState(true);
   const [location, setLocation] = useState("");
   const [checkin, setCheckin] = useState("");
   const [checkout, setCheckout] = useState("");
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const [guest, setGuest] = useState(1);
+  const handleForm = () => {
+    const newInfo = [
+      { location: location, chekin: checkin, checkout: checkout, guest: guest },
+    ];
+    console.log(newInfo);
+    setLocation("");
+    setCheckout("");
+    setCheckin("");
+    setGuest(1);
     setLoad(false);
   };
   return (
     <div className="py-3">
       <div className="container">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(handleForm)}>
           <div className="row">
-            <div className="col-md-3">
+            <div className="col-md-2">
               <div className="form-group">
                 <label>Location</label>
                 <input
                   type="text"
-                  placeholder="Where you are going?"
+                  placeholder="Destination?"
                   className="form-control"
+                  name="destination"
+                  ref={register({
+                    required: "Required",
+                  })}
+                  value={location}
+                  onChange={(e) => setLocation(e.target.value)}
                 />
               </div>
+              <p className="text-danger">
+                {errors.destination && errors.destination.message}
+              </p>
             </div>
             <div className="col-md-3">
               <div className="form-group">
                 <label>Check-in date</label>
-                <input type="date" className="form-control" />
+                <input
+                  type="date"
+                  className="form-control"
+                  name="checkin"
+                  ref={register({
+                    required: "Required",
+                  })}
+                  value={checkin}
+                  onChange={(e) => setCheckin(e.target.value)}
+                />
               </div>
+              <p className="text-danger">
+                {errors.checkin && errors.checkin.message}
+              </p>
             </div>
             <div className="col-md-3">
               <div className="form-group">
                 <label>Check-out date</label>
-                <input type="date" className="form-control" />
+                <input
+                  type="date"
+                  className="form-control"
+                  name="checkout"
+                  ref={register({
+                    required: "Required",
+                  })}
+                  value={checkout}
+                  onChange={(e) => setCheckout(e.target.value)}
+                />
+              </div>
+              <p className="text-danger">
+                {errors.checkout && errors.checkout.message}
+              </p>
+            </div>
+            <div className="col-md-2">
+              <div className="form-group">
+                <label>No. of guest</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  value={guest}
+                  onChange={(e) => setGuest(guest + 1)}
+                />
               </div>
             </div>
-            <div className="col-md-3">
+            <div className="col-md-2">
               <div className="form-group">
                 <div className="search-btn">
                   <button className="btn btn-danger">Search</button>
@@ -51,6 +105,7 @@ const Search = () => {
       </div>
       <div className=" container py-5">
         {load ? <Banner></Banner> : <Result></Result>}
+        {/* {load ? "" : <Result></Result>} */}
       </div>
     </div>
   );
